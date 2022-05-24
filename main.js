1dac339c2f3de0acba8ceb84dcfb5fbb7fa6bac7
Ilya <mac@MacBookPro.local>
2022-05-24 14:35:26 +0300

    add star

--- main.js
+++ main.js
@@ -1,106 +1,101 @@
 (() => {
 	const width = window.innerWidth;
 	const height = window.innerHeight;
+	let loadWorld = false;
 
 	const gui = new dat.GUI();
 
-	const sphere = {
+	const planeta = {
 		positionX: 0,
 		positionY: 0,
 		positionZ: 0,
-		rotationX: 0,
+		rotationX: 0.001,
 		rotationY: 0,
-		rotationZ: 0,
+		rotationZ: 0.001,
 	};
 
-	gui.add(sphere, 'positionX').min(-1).max(1).step(0.01);
-	gui.add(sphere, 'positionY').min(-1).max(1).step(0.01);
-	gui.add(sphere, 'positionZ').min(-5).max(5).step(0.1);
+	const cameraAnimation = {
+		rotationX: 0.001,
+		rotationY: 0,
+		rotationZ: 0.001,
+	};
 
-	gui.add(sphere, 'rotationX').min(-0.02).max(0.02).step(0.001);
-	gui.add(sphere, 'rotationY').min(-0.02).max(0.02).step(0.001);
-	gui.add(sphere, 'rotationZ').min(-0.02).max(0.02).step(0.001);
+	gui.add(planeta, 'positionX').min(-0.01).max(0.01).step(0.001);
+	gui.add(planeta, 'positionY').min(-0.01).max(0.01).step(0.001);
+	gui.add(planeta, 'positionZ').min(-0.01).max(0.01).step(0.001);
 
-	const titleText = document.createElement('div');
-	titleText.className = 'title';
-	titleText.innerText = 'WEBGL + TreeJS';
-	document.body.appendChild( titleText );
+	gui.add(planeta, 'rotationX').min(-0.01).max(0.01).step(0.001);
+	gui.add(planeta, 'rotationY').min(-0.01).max(0.01).step(0.001);
+	gui.add(planeta, 'rotationZ').min(-0.01).max(0.01).step(0.001);
 
 	const renderer = new THREE.WebGLRenderer();
+
 	renderer.setClearColor(0x000000);
 	renderer.setSize( width, height );
-	document.body.appendChild( renderer.domElement );
+	document.body.before( renderer.domElement);
 
 	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 5000 );
 
-	const camera = new THREE.PerspectiveCamera( 45, width / height, 0.1, 5000 );
-
-	camera.position.set(150,0, 1000);
+	camera.position.set(0,0, 1000);
 
 	const light = new THREE.AmbientLight( 0xffffff );
+
 	scene.add(light);
 
-	const geometrySphere = new THREE.SphereGeometry( 150, 150, 20, 20 ); // Ширина, высота, количество клеточек на плоскости
-	const materialSphere = new THREE.MeshBasicMaterial({
-		color: 0x00ff00,
-		wireframe: true
+	const geometryPlanet = new THREE.SphereGeometry( 150, 150, 20, 20 );
+	const planetTexture = new THREE.TextureLoader().load('./images/textures/planet.jpg');
+
+	const materialPlanet = new THREE.MeshBasicMaterial({ map: planetTexture, overdraw: true });
+	const meshPlanet = new THREE.Mesh(geometryPlanet, materialPlanet);
+	scene.add(meshPlanet);
+
+	let starGeometry = new THREE.BoxGeometry();
+	const starTexture = new THREE.TextureLoader().load('./images/textures/star.png');
+	const startMaterial = new THREE.PointsMaterial({
+		color: 0xaaaaaa,
+		size: 0.7,
+		map: starTexture
 	});
-	const meshSphere = new THREE.Mesh(geometrySphere, materialSphere);
-	meshSphere.position.y = -150;
-	scene.add(meshSphere);
+	const starsPoints = new THREE.Points(starGeometry, startMaterial);
+	scene.add(starsPoints);
 
-	const geometrySphereTX = new THREE.SphereGeometry( 150, 150, 20, 20 );
+	setTimeout(() => {
+		loadWorld = true;
+	}, 10000);
 
-	let sphereTexture = new THREE.Texture();
-	let sphereLoader = new THREE.ImageLoader();
-
-	sphereLoader.load(
-			'./images/textures/planet.jpg',
-			( image ) => {
-				sphereTexture.image = image;
-				sphereTexture.needsUpdate = true;
-			}
-	);
-
-	const materialSphereTX = new THREE.MeshBasicMaterial({ map: sphereTexture, overdraw: true });
-	const meshSphereTX = new THREE.Mesh(geometrySphereTX, materialSphereTX);
-	meshSphereTX.position.y = 200;
-	scene.add(meshSphereTX);
-
-	const geometryCube = new THREE.BoxGeometry(150, 150, 150);
-
-	let cubeTexture = new THREE.Texture();
-	let loader = new THREE.ImageLoader();
-
-	loader.load(
-			'./images/textures/box.jpg',
-			 ( image ) => {
-				cubeTexture.image = image;
-				cubeTexture.needsUpdate = true;
-			}
-	);
-
-	const materialCube = new THREE.MeshBasicMaterial({ map: cubeTexture, overdraw: true });
-	const meshCube = new THREE.Mesh(geometryCube, materialCube);
-	meshCube.position.x = 150 * 2.5;
-	meshCube.rotation.y = width / 3;
-	scene.add(meshCube);
+	if (loadWorld) {
+		camera.position.set(
+				cameraAnimation.rotationX,
+				cameraAnimation.rotationY,
+				cameraAnimation.rotationZ
+		);
+	}
 
 	function animate() {
+		starGeometry.velocity += 1000;
+		starGeometry.acceleration += 2000;
+		starGeometry.verticesNeedUpdate = true;
+		starsPoints.rotation.y += 0.1;
+		starsPoints.rotation.z += 0.1;
 		requestAnimationFrame( animate );
 
-		meshCube.rotation.z += 0.01;
+		meshPlanet.rotation.x += planeta.positionX;
+		meshPlanet.rotation.y += planeta.positionY;
+		meshPlanet.rotation.z += planeta.positionZ;
 
-		meshSphereTX.rotation.x += 0.001;
-		meshSphereTX.rotation.y += 0.001;
-		meshSphereTX.rotation.z += 0.001;
+		meshPlanet.rotation.x += planeta.rotationX;
+		meshPlanet.rotation.y += planeta.rotationY;
+		meshPlanet.rotation.z += planeta.rotationZ;
 
-		meshSphere.position.x += sphere.positionX;
-		meshSphere.position.y += sphere.positionY;
-		meshSphere.position.z += sphere.positionZ;
-		meshSphere.rotation.x += sphere.rotationX;
-		meshSphere.rotation.y += sphere.rotationY;
-		meshSphere.rotation.z += sphere.rotationZ;
+		if (!loadWorld) {
+			loadWorld = false;
+			camera.position.set(
+					cameraAnimation.rotationX += 0.1,
+					cameraAnimation.rotationY += 0.1,
+					cameraAnimation.rotationZ += 1
+			);
+		}
 
 		renderer.render( scene, camera );
 	}
